7aeb9b56c6a1fe77f28b29af944b2b0c7840f2f8
mihran1001 <mihrannersessyan@mail.ru>
2025-01-14 23:26:28 +0400

    some changes and addings

--- dx-soft-project/src/Components/PracticeAdvice/PracticeAdvice.js
+++ dx-soft-project/src/Components/PracticeAdvice/PracticeAdvice.js
@@ -1,10 +1,15 @@
 import "./PracticeAdvice.css";
+import React from "react";
+import star from "../../icons/staricon.jpg";
+import emptystar from "../../icons/emptystaricon.jpg";
+import pic1 from "../../pics/pic1.png";
+import pic2 from "../../pics/pic2.png";
 
 function PracticeAdvice(){
-    return (<div class="PracticeAdvice">
-        <div class="container">
-            <div class="header">
-                <div class="title">
+    return (<div className="PracticeAdvice">
+        <div className="container">
+            <div className="header">
+                <div className="title">
                     <h6>Practice Advice</h6>
                     <h2>Each and every client is important</h2>
                     <p>Problems trying to resolve the conflict between 
@@ -12,19 +17,36 @@ function PracticeAdvice(){
                 </div>
             </div>
         </div>
-            <div class="footer">
-                <div class='carousel'>
-                    <div class="comment1">comment1</div>
-                    <div class="comment2">
-                        <div class="picture">
-                            <img src="../pics/pic1.png" alt="Girl in a jacket" /> 
+            <div className="footer">
+                <div className='carousel'>
+                    <div className="comment1">comment1</div>
+                    <div className="comment2">
+                        <div className="image1">
+                            <img src={pic1} alt="pic1" /> 
                         </div>
-                        <div class="content">
-                            Slate helps you see  how many more days you need to work to reach your financial goal for the month and year.
+                        <div className="content">
+                            <span>Slate helps you see  how many more days you need to work to reach your financial goal for the month and year.</span>
+                            <img src={star} alt="Icon of a star" />
+                            <img src={star} alt="Icon of a star" />
+                            <img src={star} alt="Icon of a star" />
+                            <img src={star} alt="Icon of a star" />
+                            <img src={emptystar} alt="Icon of an empty star" />
                         </div>
                     </div>
-                    <div class="comment3">Slate helps you see  how many more days you need to work to reach your financial goal for the month and year.</div>
-                    <div class="comment4">comment4</div>
+                    <div className="comment3">
+                        <div className="image2">
+                            <img src={pic2} alt="pic2" />
+                        </div>
+                        <div>
+                            <span>Slate helps you see  how many more days you need to work to reach your financial goal for the month and year.</span>
+                            <img src={star} alt="Icon of a star" />
+                            <img src={star} alt="Icon of a star" />
+                            <img src={star} alt="Icon of a star" />
+                            <img src={star} alt="Icon of a star" />
+                            <img src={emptystar} alt="Icon of an empty star" />
+                        </div>
+                    </div>
+                    <div className="comment4">comment4</div>
                 </div>
             </div> 
     </div>);
