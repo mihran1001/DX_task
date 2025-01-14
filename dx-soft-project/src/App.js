38d4ec63cc1a78e5aed9ac9a5275e04df37c6707
mihran1001 <mihrannersessyan@mail.ru>
2025-01-14 19:55:14 +0400

    first steps

---
+++ dx-soft-project/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import PracticeAdvice from './Components/PracticeAdvice/PracticeAdvice.js';
+
+function App() {
+  return (
+    <div className="App">
+      <PracticeAdvice />
+      {/* <Subscribe />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ dx-soft-project/src/Components/PracticeAdvice/PracticeAdvice.js
@@ -0,0 +1,33 @@
+import "./PracticeAdvice.css";
+
+function PracticeAdvice(){
+    return (<div class="PracticeAdvice">
+        <div class="container">
+            <div class="header">
+                <div class="title">
+                    <h6>Practice Advice</h6>
+                    <h2>Each and every client is important</h2>
+                    <p>Problems trying to resolve the conflict between 
+                    the two major realms of Classical physics: Newtonian mechanics </p>
+                </div>
+            </div>
+        </div>
+            <div class="footer">
+                <div class='carousel'>
+                    <div class="comment1">comment1</div>
+                    <div class="comment2">
+                        <div class="picture">
+                            <img src="../pics/pic1.png" alt="Girl in a jacket" /> 
+                        </div>
+                        <div class="content">
+                            Slate helps you see  how many more days you need to work to reach your financial goal for the month and year.
+                        </div>
+                    </div>
+                    <div class="comment3">Slate helps you see  how many more days you need to work to reach your financial goal for the month and year.</div>
+                    <div class="comment4">comment4</div>
+                </div>
+            </div> 
+    </div>);
+}
+
+export default PracticeAdvice;
