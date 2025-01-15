8fa1b59e5f3365ce83fb0c33fab5a68d56ebb50f
mihran1001 <mihrannersessyan@mail.ru>
2025-01-15 15:57:11 +0400

    changing js files into jsx and working on footer section

--- dx-soft-project/src/App.js
+++ dx-soft-project/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import React from 'react';
-import Subscribe from './Components/Subscribe/Subscribe.js';
-import PracticeAdvice from './Components/PracticeAdvice/PracticeAdvice.js';
+import Subscribe from './Components/Subscribe/Subscribe.jsx';
+import PracticeAdvice from './Components/PracticeAdvice/PracticeAdvice.jsx';
+import Footer from './Components/Footer/Footer.jsx';
 
 function App() {
   return (
     <div className="App">
       <PracticeAdvice />
       <Subscribe />
-      {/* <Footer /> */}
+      <Footer /> 
     </div>
   );
 }
---
+++ dx-soft-project/src/Components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import "./Footer.css";
+
+function Footer(){
+    return (<div className="footer-section">
+        <div className="info">
+            <div className="first">
+                <h3>Get In Touch</h3>
+                <p>the quick fox jumps over the lazy dog</p>
+            </div>
+            <div className="second">
+                <h3>Company info</h3>
+                <a href="http://facebook.com">About Us</a>
+                <a href="http://facebook.com">Carrier</a>
+                <a href="http://facebook.com">We are hiring</a>
+                <a href="http://facebook.com">Blog</a>
+            </div>
+            <div className="third">
+                <h3>Features</h3>
+                <a href="http://facebook.com">Business Marketing</a>
+                <a href="http://facebook.com">User Analytic</a>
+                <a href="http://facebook.com">Live Chat</a>
+                <a href="http://facebook.com">Unlimited Support</a>
+            </div>
+            <div className="fourth">
+                <h3>Resources</h3>
+                <a href="http://facebook.com">IOS & Android</a>
+                <a href="http://facebook.com">Watch a Demo</a>
+                <a href="http://facebook.com">Customers</a>
+                <a href="http://facebook.com">API</a>
+            </div>
+        </div>
+        <div className="signature">
+
+        </div>
+        <div className="background">
+            <h6>Made With Love By Figmaland All Right Reserved</h6> 
+        </div>
+    </div>)
+}
+
+export default Footer;
