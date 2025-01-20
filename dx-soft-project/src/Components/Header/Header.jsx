5990ec860d7403b4c8bbcf5eeb0c07e7562c5775
Arman-Haroyan <arman.haroyann@gmail.com>
2025-01-20 11:02:46 +0400

    refresh

--- dx-soft-project/src/Components/Header/Header.jsx
+++ dx-soft-project/src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import "./Header.css";
+import styles from "./Header.module.css";
 import hero from "../../images/heroimage.png"
 import rectangle from "../../images/rectangle.png"
 import smallcard from "../../images/smallcard.png"
@@ -10,69 +10,71 @@ import line1 from "../../images/line1.png"
 import line2 from "../../images/line2.png"
 import line3 from "../../images/line3.png"
 import vector from "../../images/vector.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
     return (     
         
-    <div className="section_one">
-        <img src={vector} className="vector"/> 
-        <div className="header">
-            <div className="menu">
-                <span className="logo">BrandName</span>
-                <ul className="menu_list">
-                    <li>Home</li>
-                    <li>Product</li>
-                    <li>Pricing</li>
-                    <li>Contact</li>
-                </ul>
+    <div className={styles.section_one}>
+        <img src={vector} className={styles.vector} alt="vector"/> 
+        <div className={styles.header}>
+            <div className={styles.menu}>
+                <span className={styles.logo}>BrandName</span>
+                    <ul className={styles.menu_list}>
+                        <li>Home</li>
+                        <li>Product</li>
+                        <li>Pricing</li>
+                        <li>Contact</li>
+                    </ul>
             </div>
-            <div className="right_side_menu">
-                <ul className="right_side_menu_list">
-                    <li>Login</li>
-                </ul>
-                <button>JOIN US <i class="fa-solid fa-arrow-right"></i></button>
-                <img src={rectangle} alt="rectangle" className="forimage2"/>
-                <img src={hero} alt="hero" className="forimage"/>
-                <img src={smallcard} alt="smallcard" className="forimage3"/>
-                <img src={bigcard} alt="bigcard" className="forimage4"/>
-            </div>
+                <div className={styles.right_side_menu}>
+                    <ul className={styles.right_side_menu_list}>
+                        <li>Login</li>
+                    </ul>
+                    <button>JOIN US <FontAwesomeIcon icon={faArrowRight}/></button>
+                    <img src={rectangle} alt="rectangle" className={styles.forimage2}/>
+                    <img src={hero} alt="hero" className={styles.forimage}/>
+                    <img src={smallcard} alt="smallcard" className={styles.forimage3}/>
+                    <img src={bigcard} alt="bigcard" className={styles.forimage4}/>
+                </div>
             
         </div>
 
-        <div className="backGround"> 
-                <div className="text1">Online training</div>
-                <div className="text2">25K+ STUDENTS <br></br>TRUST US</div>
-                <div className="text3">We know how large objects will act, 
-                <br></br>but things on a small scale</div>
-                <button className="secondbutton">Join us</button>
-                <button className="thirdbutton">Learn More</button>
-        </div>
+            <div className={styles.backGround}> 
+                    <div className={styles.text1}>Online training</div>
+                    <div className={styles.text2}>25K+ STUDENTS <br></br>TRUST US</div>
+                    <div className={styles.text3}>We know how large objects will act, 
+                    <br></br>but things on a small scale</div>
+                    <button className={styles.secondbutton}>Join us</button>
+                    <button className={styles.thirdbutton}>Learn More</button>
+            </div>
         
-        <div className="container">
+        <div className={styles.container}>
                   
-                    <div className="box">
-                    <img src={icon1}/>
-                    <p className="textbold">Evaluation Time</p>
-                    <img src={line1}/>
-                    <p className="maintext">The gradual accumulation of<br></br>
+                    <div className={styles.box}>
+                    <img src={icon1} alt="icon1"/>
+                    <p className={styles.textbold}>Evaluation Time</p>
+                    <img src={line1} alt="line1"/>
+                    <p className={styles.maintext}>The gradual accumulation of<br></br>
                         information about atomic and <br></br>
                         small-scale behaviour...</p>
                     </div>
 
-                    <div className="box">
-                    <img src={icon2}/>
-                    <p className="textbold">Certified Teacher</p>
-                    <img src={line2}/>
-                    <p className="maintext">The gradual accumulation of<br></br>
+                    <div className={styles.box}>
+                    <img src={icon2} alt="icon2"/>
+                    <p className={styles.textbold}>Certified Teacher</p>
+                    <img src={line2} alt="line2"/>
+                    <p className={styles.maintext}>The gradual accumulation of<br></br>
                         information about atomic and <br></br>
                         small-scale behaviour...</p>
                     </div>
                          
-                    <div className="box">
-                    <img src={icon3}/>
-                    <p className="textbold">Books Liberary</p>
-                    <img src={line3}/>
-                    <p className="maintext">The gradual accumulation of <br></br>
+                    <div className={styles.box}>
+                    <img src={icon3} alt="icon3"/>
+                    <p className={styles.textbold}>Books Liberary</p>
+                    <img src={line3} alt="line3"/>
+                    <p className={styles.maintext}>The gradual accumulation of <br></br>
                         information about atomic and <br></br>
                         small-scale behaviour...</p>
                     </div>
--- dx-soft-project/src/Components/Section2/Section2.jsx
+++ dx-soft-project/src/Components/Section2/Section2.jsx
@@ -1,31 +1,33 @@
 import styles from "./Section2.module.css"
 import heroes from "../../images/Heroes.png"
 import line from "../../images/line.png"
-import cursoricon from "../../images/cursoricon.png"
 import boximage from "../../images/boximage.png"
 import largebox from "../../images/largebox.png"
 import square from "../../images/square.png"
 import smallrectangle from "../../images/smallrectangle.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
 function Section2(){
     return (
 <div className={styles.section_two}>
     <div className={styles.forheroes}>
-         <img src={heroes} className={styles.heroes}/>
-         <img src={boximage} className={styles.boximage}/>
-         <img src={largebox} className={styles.largebox}/>
-         <img src={square} className={styles.square}/>
-         <img src={smallrectangle} className={styles.smallrectangle}/>
+         <img src={heroes} className={styles.heroes} alt="heroes"/>
+         <img src={boximage} className={styles.boximage} alt="boximage"/>
+         <img src={largebox} className={styles.largebox} alt="largebox"/>
+         <img src={square} className={styles.square} alt="square"/>
+         <img src={smallrectangle} className={styles.smallrectangle} alt="smallrectangle"/>
     </div>
     
-    <div className={styles.forP}>
-        <p className={styles.p1}>Make online education<br></br>
-        accessible</p>
-        <img src={line} className={styles.line}/>
-        <p className={styles.p2}>Problems trying to resolve the conflict between<br></br> 
-        the two major realms of Classical physics:<br></br> 
-        Newtonian mechanics </p>
-        <li className={styles.button}>Learn More</li>
-    </div>
+        <div className={styles.forP}>
+            <p className={styles.p1}>Make online education<br></br>
+            accessible</p>
+            <img src={line} className={styles.line} alt="line"/>
+            <p className={styles.p2}>Problems trying to resolve the conflict between<br></br> 
+            the two major realms of Classical physics:<br></br> 
+            Newtonian mechanics </p>
+            <li className={styles.button}>Learn More<FontAwesomeIcon icon={faAngleRight} /></li>
+        </div>
 </div>
     )
 }
